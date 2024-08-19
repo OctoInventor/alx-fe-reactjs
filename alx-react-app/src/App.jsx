f210f47eecddece6b2df3abe4e5346abc7a02242
OctoInventor <danielappiahagyei503@gmail.com>
2024-08-19 00:40:28 +0000

    upload

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './WelcomeMessage'
+import WelcomeMessage from './WelcomeMessage';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,7 +29,7 @@ function App() {
           Click on the Vite and React logos to learn more
         </p>
       </div>
-      <WelcomeMessage />;
+      <WelcomeMessage />
     </>
   )
 }
