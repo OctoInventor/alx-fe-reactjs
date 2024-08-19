76a00f75a79f977d12e50afb0f44b94198352f61
OctoInventor <danielappiahagyei503@gmail.com>
2024-08-19 00:26:29 +0000

    upload

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './WelcomeMessage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,7 +18,18 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>Hello World</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test the HMR
+        </p>
+        <p className="read-the-cocs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+      <WelcomeMessage />;
     </>
   )
 }
