95b620151fa129d0e4fe27b25c0ca3a03f75e69a
OctoInventor <danielappiahagyei503@gmail.com>
2024-08-20 01:02:28 +0000

    upload

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './WelcomeMessage'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,6 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <WelcomeMessage />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,12 +26,12 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test the HMR
         </p>
-        <p className="read-the-cocs">
+        <p className="read-the-docs">
           Click on the Vite and React logos to learn more
         </p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
