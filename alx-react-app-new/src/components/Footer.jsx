e3a4a3650d906d427f4e4718977ee9cb82f7f9ae
OctoInventor <danielappiahagyei503@gmail.com>
2024-08-22 01:51:09 +0000

    uploads

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Footer = () => {
+  const footerStyle = {
+    backgroundColor: '#333',
+    color: '#fff',
+    padding: '10px 0',
+    textAlign: 'center',
+    position: 'fixed',
+    bottom: '0',
+    width: '100%',
+    boxShadow: '0 -2px 5px rgba(0, 0, 0, 0.1)',
+    fontFamily: 'Arial, sans-serif',
+  };
+
+
+function Footer () {
+    return (
+        <footer>
+        <p>© 2023 City Lovers</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const MainContent = () => {
+  const mainContentStyle = {
+    backgroundColor: '#f0f8ff',
+    padding: '20px',
+    borderRadius: '10px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    fontFamily: 'Arial, sans-serif',
+    color: '#333',
+    textAlign: 'center',
+  };
+
+function MainContent () {
+    return (
+      <main>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+    );
+  };
+  
+  export default MainContent;
