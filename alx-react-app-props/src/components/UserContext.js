4b50b983a2ae40d8ed835a6973c5820dda2b46cf
OctoInventor <danielappiahagyei503@gmail.com>
2024-08-25 01:24:09 +0000

    uploads

---
+++ alx-react-app-props/src/components/UserContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// Initialize the UserContext
+const UserContext = React.createContext();
+
+// Export the UserContext
+export default UserContext;
---
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import UserInfo from './UserInfo';
+import UserContext from './UserContext';
+
+const UserDetails = () => {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>User Details</h2>
+      <UserInfo userData={userData} />
+    </div>
+  );
+};
+
+export default UserDetails;
