8aa6b848171d4e086d24d5eef95ef5786a9f7b43
OctoInventor <danielappiahagyei503@gmail.com>
2024-08-25 01:54:30 +0000

    uploads

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,7 +3,7 @@ import ProfilePage from './components/ProfilePage';
 import UserContext from './components/UserContext';
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com", bio:"A software developer from NY"};
 
   return (
     <UserContext.Provider value={userData}>
--- alx-react-app-props/src/components/UserInfo.jsx
+++ alx-react-app-props/src/components/UserInfo.jsx
@@ -8,6 +8,7 @@ const UserInfo = () => {
     <div>
       <p>Name: {userData.name}</p>
       <p>Email: {userData.email}</p>
+      <p>Bio: {userData.bio}</p>
     </div>
   );
 };
