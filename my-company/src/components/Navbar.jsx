b9853581959483c95dc64e4e397a352cf07dc4b9
OctoInventor <danielappiahagyei503@gmail.com>
2024-08-28 21:49:13 +0000

    updated with css

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ const Navbar = () => {
   const navStyle = {
     display: 'flex',
     justifyContent: 'space-between',
+    alignItems: 'center', // Center vertically
     padding: '10px',
     backgroundColor: '#333',
     color: 'white',
@@ -16,11 +17,16 @@ const Navbar = () => {
     color: 'white',
     textDecoration: 'none',
     fontSize: '18px',
+    transition: 'color 0.3s ease', // Smooth color transition
+  };
+
+  const activeLinkStyle = {
+    color: '#f9a825', // Highlight active link
   };
 
   return (
     <nav style={navStyle}>
-      <Link to="/" style={linkStyle}>Home</Link>
+      <Link to="/" style={{ ...linkStyle, ...activeLinkStyle }}>Home</Link>
       <Link to="/about" style={linkStyle}>About</Link>
       <Link to="/services" style={linkStyle}>Services</Link>
       <Link to="/contact" style={linkStyle}>Contact</Link>
