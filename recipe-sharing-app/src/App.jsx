f1f934731ae95c7e38039b56a770c9d6199f0043
OctoInventor <danielappiahagyei503@gmail.com>
2024-09-03 23:55:19 +0000

    uploads

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importing Router, Route, and Routes from react-router-dom
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-
 
 function App() {
   const [recipes, setRecipes] = useState([]);
@@ -11,11 +11,15 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>My Recipe App</h1>
-      <AddRecipeForm addRecipe={addRecipe} />
-      <RecipeList recipes={recipes} />
-    </div>
+    <Router>
+      <div>
+        <h1>My Recipe App</h1>
+        <Routes>
+          <Route path="/" element={<RecipeList recipes={recipes} />} />
+          <Route path="/add" element={<AddRecipeForm addRecipe={addRecipe} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
