d787013e70c5a5d6e8fe5c8ea5bb8cc8c92c7fe2
OctoInventor <danielappiahagyei503@gmail.com>
2024-09-03 02:55:40 +0000

    uploads

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -7,6 +7,10 @@ function RecipeList() {
   return (
     <div>
       <h2>Recipe List</h2>
+      <h4> Dinner Recipes · Fruits, Vegetables and Other Produce · Bread Recipes · Everyday </h4>
+      <h4> Lunch Recipes · Bring on the classics, like a tuna salad or grilled cheese sandwich, or make a delicious soup or salad</h4>
+      <h4> Appetizers and Snacks · Perfect party appetizers made easy. See hundreds of tasty appetizers with photos, helpful reviews, 
+        and tips on how to make them</h4>
       <ul>
         {recipes.map((recipe, index) => (
           <li key={index}>
