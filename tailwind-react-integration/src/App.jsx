6530c3c332fc69d4fbf2972a184fbcb02930d804
OctoInventor <danielappiahagyei503@gmail.com>
2024-09-17 00:35:25 +0000

    upload files

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import './App.css'
-import UserProfile from './UserProfile'
+//import './App.css'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,9 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <a>
           <UserProfile/>
-        </a>
       </div>
     </>
   )
