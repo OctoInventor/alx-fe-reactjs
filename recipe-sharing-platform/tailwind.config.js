e72882f1a422d71c79eecea0b92369a118c0bc55
OctoInventor <danielappiahagyei503@gmail.com>
2024-09-18 01:27:27 +0000

    uploads

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "class",
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: [],
+};
