56e868b06f9f4f914ccedd002d9fae2dd00c7348
OctoInventor <danielappiahagyei503@gmail.com>
2024-09-18 00:48:49 +0000

    upload files

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 function UserProfile() {
     return (
-        <div className="bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+        <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
            <img
-                src="profle-image.jpg"
+                src="./src/assets/proimage.jpg"
                 alt="profile-image"
-                className="w-24 h-24 sm:w-36 sm:h-36 max-auto" />
+                className="w-24 h-24 sm:w-36 sm:h-36 mx-auto" />
             <h1 className="text-lg sm:text-xl my-4">John Doe</h1>
             <p className="text-sm sm:text-base">
                 Web Developer at XYZ Co. Loves to write code and explore new technologies
