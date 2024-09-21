b1a70bb0406be7f44a173a3ae2d742690fafd531
OctoInventor <danielappiahagyei503@gmail.com>
2024-09-21 02:07:10 +0000

    uploads

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import RecipeList from './components/RecipeList.jsx';
-import HomePage from './components/HomePage.jsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
